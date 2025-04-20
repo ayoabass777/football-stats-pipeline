ca430d7b6715b01c0316ac481342fa404f01b4e1
Ayomide Abass <ayoabass777@gmail.com>
2025-04-20 05:08:08 -0700

    changed name mart_team_results_streaks.sql to mart_team_result_streaks.sql

---
+++ football_pipeline/models/marts/mart_team_result_streaks.sql
@@ -0,0 +1,76 @@
+{{ config(materialized='table') }}
+
+WITH base AS (
+    SELECT *
+    FROM {{ ref('int_team_match_results')}}
+),
+
+-- Unpivot all indicators into (team_id, date, fixtures,..., event_name, event_flag)
+event_indicator AS (
+    SELECT 
+        b.fixture_id,
+        b.date,
+        b.team_id,
+        b.team_name,
+        b.result,
+        ev.event_name AS event_name,
+        --convert ev_flag to int from boolean
+        ev.event_flag:: int AS event_flag
+    FROM base AS b
+    CROSS JOIN LATERAL (
+        VALUES
+            ('win', b.result = 'win'),
+            ('draw', b.result = 'draw'),
+            ('loss', b.result = 'loss'),
+
+            ('win_or_draw', b.result in ('win','draw')),
+            ('loss_or_draw', b.result in ('loss','draw'))
+    ) AS ev(event_name, event_flag) -- ev means event
+),
+
+-- Compute the lag of each event per team and event name
+lag_event_indicator AS (
+    SELECT *,
+        LAG(event_flag,1,0) OVER(PARTITION BY team_id, event_name ORDER BY date) AS lag_ev_flag
+    FROM event_indicator
+),
+
+-- Build a running group ID for each new streak
+streak_grouped AS (
+    SELECT *,
+        -- start a new group whenever ev_flag = 1 and prior was 0
+        SUM(
+            CASE 
+                WHEN event_flag = 1 AND lag_ev_flag = 0 THEN 1 
+                ELSE 0 
+            END
+            ) OVER (PARTITION BY team_id, event_name ORDER BY date) AS streak_grp
+    FROM lag_event_indicator
+),
+
+streaks AS (
+    SELECT *,
+        -- count rows within each (team_id, event_name, streak_grp) for which ev_flag =1 (True)
+        ROW_NUMBER() OVER (PARTITION BY team_id, event_name, streak_grp ORDER BY date) AS streak_count
+    FROM streak_grouped
+    WHERE event_flag = 1
+),
+
+final AS (
+    SELECT 
+        Fixture_id,
+        date,
+        team_id,
+        team_name,
+        result,
+        event_name,
+        event_flag,
+        lag_ev_flag,
+        streak_grp,
+        streak_count
+
+    FROM streaks
+    ORDER BY event_name, date
+)
+
+SELECT * FROM final
