22339d48d9322ba744bc8e1010d604eb072e13a6
Ayomide Abass <ayoabass777@gmail.com>
2025-10-30 12:35:18 -0700

    fixed broken api

--- API/main.py
+++ API/main.py
@@ -5,8 +5,15 @@ from typing import List, Literal
 from sqlalchemy import create_engine, Column, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 import os
-DATABASE_URL = "postgresql://ayoabass:tamzynana@localhost:5432/football_betting"
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME")
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
 
 
 engine = create_engine(DATABASE_URL)
